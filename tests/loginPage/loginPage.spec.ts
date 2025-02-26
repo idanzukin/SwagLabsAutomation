fc3467ba5ad2d0e420475256baa3aee02d8ce7d4
zukin <ididan1233@gmail.com>
2025-02-26 18:01:03 +0200

    insert login test with invalid username or invalid password

--- tests/loginPage/loginPage.spec.ts
+++ tests/loginPage/loginPage.spec.ts
@@ -7,20 +7,58 @@ import {
   userNameInputId,
 } from "../../selectors/loginPage/loginPage";
 
-
 test("Login to Swag Labs with valid credentials", async ({ page }) => {
-  //Arrange
   const userName = "standard_user";
   const password = "secret_sauce";
   const urlExpected = "https://www.saucedemo.com/inventory.html";
 
-  //Act
   await page.goto(config.SwagLabsUrl);
   await getObjectById(page, userNameInputId).fill(userName);
   await getObjectById(page, passwordInputId).fill(password);
   await getObjectById(page, loginButtonId).click();
 
-  //Assert
+  expect(page.url()).toBe(urlExpected);
+  page.close();
+});
+
+test("Login to Swag Labs with invalid credentials", async ({ page }) => {
+  const userName = "hello";
+  const password = "world";
+  const urlExpected = "https://www.saucedemo.com/inventory.html";
+
+  await page.goto(config.SwagLabsUrl);
+  await getObjectById(page, userNameInputId).fill(userName);
+  await getObjectById(page, passwordInputId).fill(password);
+  await getObjectById(page, loginButtonId).click();
+
+  expect(page.url()).toBe(urlExpected);
+  page.close();
+});
+
+test("Login to Swag Labs with invalid password", async ({ page }) => {
+  const userName = "standard_user";
+  const password = "world";
+  const urlExpected = "https://www.saucedemo.com/inventory.html";
+
+  await page.goto(config.SwagLabsUrl);
+  await getObjectById(page, userNameInputId).fill(userName);
+  await getObjectById(page, passwordInputId).fill(password);
+  await getObjectById(page, loginButtonId).click();
+
+  expect(page.url()).toBe(urlExpected);
+  page.close();
+});
+
+test("Login to Swag Labs with invalid username", async ({ page }) => {
+  const userName = "world";
+  const password = "secret_sauce";
+  const urlExpected = "https://www.saucedemo.com/inventory.html";
+
+  await page.goto(config.SwagLabsUrl);
+  await getObjectById(page, userNameInputId).fill(userName);
+  await getObjectById(page, passwordInputId).fill(password);
+  await getObjectById(page, loginButtonId).click();
+
   expect(page.url()).toBe(urlExpected);
   page.close();
 });
